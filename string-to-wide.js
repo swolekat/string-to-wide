6eac3d46b8e19a26b4c27e54f59be399d8ae9cec
swolekat <swolecattwitch@gmail.com>
2023-02-15 17:12:04 -0600

    remove comment

--- string-to-wide.js
+++ string-to-wide.js
@@ -1,8 +1,6 @@
 const stringToWide = (str) => {
     return str.split('').map((char) => {
         const code = char.charCodeAt(0);
-        // ff10
-        // 48 to
         const wideCode = code + 65248;
         return String.fromCharCode(wideCode);
     }).join('');
